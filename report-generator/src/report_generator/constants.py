b7b55ffc5c716dffdc915227868462b99f8c87ad
florisvanleeuwen <floris.vanleeuwen@softwareimprovementgroup.com>
2025-03-18 13:23:33 +0100

    Apply copyright to report generator files

--- report-generator/src/report_generator/constants.py
+++ report-generator/src/report_generator/constants.py
@@ -1,3 +1,17 @@
+#  Copyright Software Improvement Group
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 MAINT_METRICS = ["VOLUME", "DUPLICATION", "UNIT_SIZE", "UNIT_COMPLEXITY", "UNIT_INTERFACING", "MODULE_COUPLING",
                  "COMPONENT_INDEPENDENCE", "COMPONENT_ENTANGLEMENT"]
 ARCH_METRICS = ["CODE_BREAKDOWN", "COMPONENT_COUPLING", "COMPONENT_COHESION", "CODE_REUSE",
