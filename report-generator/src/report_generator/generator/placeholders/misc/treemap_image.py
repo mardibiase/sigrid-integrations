8e39e68e5c6c4c2762de18af3e44a05eabc0aa95
Thomas Kovac <t.kovac@sig.eu>
2025-08-06 12:09:27 +0200

    Placeholders will determine size and position of treemaps + add test code ratio for portfolios

--- report-generator/src/report_generator/generator/placeholders/misc/treemap_image.py
+++ report-generator/src/report_generator/generator/placeholders/misc/treemap_image.py
@@ -25,6 +25,7 @@ from report_generator.generator.placeholders.base import Placeholder
 import plotly.express as px
 import io
 from pptx.util import Inches
+from functools import cached_property
 
 class _AbstractTreemapPlaceholder(Placeholder, ABC):
     """Fills this rating value, but also colors the shape the placeholder is in to correspond to the correct maintainability rating color (e.g. yellow for 3 stars)."""
@@ -36,9 +37,36 @@ class _AbstractTreemapPlaceholder(Placeholder, ABC):
     FOUR_STAR_COLOR = "#57c968"
     FIVE_STAR_COLOR = "#2c963f"
     SIG_BLUE = "#243549"
-
-    TREEMAP_WIDTH_PIXELS = 1900
-    TREEMAP_HEIGHT_PIXELS = 617
+    
+    def translate_star_rating_to_color(rating):
+        if rating == None:
+            return _AbstractTreemapPlaceholder.NA_STAR_COLOR
+        rating = int(rating*10)/10
+        if rating < 1.5:
+            return _AbstractTreemapPlaceholder.ONE_STAR_COLOR
+        elif rating < 2.5:
+            return _AbstractTreemapPlaceholder.TWO_STAR_COLOR
+        elif rating < 3.5:
+            return _AbstractTreemapPlaceholder.THREE_STAR_COLOR
+        elif rating < 4.5:
+            return _AbstractTreemapPlaceholder.FOUR_STAR_COLOR
+        else:
+            return _AbstractTreemapPlaceholder.FIVE_STAR_COLOR
+    
+    def translate_test_code_ratio_to_color(rating):
+        if rating == None:
+            return _AbstractTreemapPlaceholder.NA_STAR_COLOR
+        rating = float(rating)
+        if rating < 0.01:
+            return _AbstractTreemapPlaceholder.ONE_STAR_COLOR
+        elif rating < 0.15:
+            return _AbstractTreemapPlaceholder.TWO_STAR_COLOR
+        elif rating < 0.50:
+            return _AbstractTreemapPlaceholder.THREE_STAR_COLOR
+        elif rating < 1.50:
+            return _AbstractTreemapPlaceholder.FOUR_STAR_COLOR
+        else:
+            return _AbstractTreemapPlaceholder.FIVE_STAR_COLOR
 
     @classmethod
     def resolve_pptx(cls, presentation: Presentation, key: str, value_cb: Callable):
@@ -47,24 +75,12 @@ class _AbstractTreemapPlaceholder(Placeholder, ABC):
             return
 
         for slide in slides:
-            paragraphs = report_utils.pptx.find_text_in_slide(slide, key)
-            for paragraph in paragraphs:
-                value_cb({'parameter' : paragraph.text, 'slide' : slide})
-
-        # rating = value_cb()
-
-        # rating_color = report_utils.pptx.determine_rating_color(rating)
-        # rating_rounded = formatters.maintainability_round(rating)
-
-        # for shape in shapes:
-        #     report_utils.pptx.set_shape_color(shape, rating_color)
-
-        # report_utils.pptx.update_many_paragraphs(paragraphs, key, rating_rounded)
+            shapes = report_utils.pptx.find_shapes_with_text_in_slide(slide, key)
+            for shape in shapes:
+                value_cb({'shape' : shape, 'slide' : slide})
 
 
-class MaintainabilityPortfolioTreemapPlaceholder(_AbstractTreemapPlaceholder):
-    key = "INTERVAL_PORTFOLIO_MAINTAINABILITY"
-
+class _AbstractPortfolioPlaceholder(_AbstractTreemapPlaceholder):
     def create_portfolio():
         res = {}
         system_names = maintainability_portfolio_data.system_names
@@ -80,7 +96,7 @@ class MaintainabilityPortfolioTreemapPlaceholder(_AbstractTreemapPlaceholder):
             }
         return res
     
-    def create_portfolio_treemap():
+    def create_blank_portfolio_treemap():
         names = []
         parents = []
         tracking = []
@@ -104,74 +120,85 @@ class MaintainabilityPortfolioTreemapPlaceholder(_AbstractTreemapPlaceholder):
             'parents' : parents,
             'tracking' : tracking
         }
-
-    def translate_star_rating_to_color(rating):
-        rating = int(rating*10)/10
-        if rating < 1.5:
-            return MaintainabilityPortfolioTreemapPlaceholder.ONE_STAR_COLOR
-        elif rating < 2.5:
-            return MaintainabilityPortfolioTreemapPlaceholder.TWO_STAR_COLOR
-        elif rating < 3.5:
-            return MaintainabilityPortfolioTreemapPlaceholder.THREE_STAR_COLOR
-        elif rating < 4.5:
-            return MaintainabilityPortfolioTreemapPlaceholder.FOUR_STAR_COLOR
-        else:
-            return MaintainabilityPortfolioTreemapPlaceholder.FIVE_STAR_COLOR
-
-    @classmethod
-    def value(cls, parameter=None):
-        # print(f"parameter: {parameter}")
-        # res = MaintainabilityPortfolioTreemapPlaceholder.create_portfolio()
-        # system_names = maintainability_portfolio_data.system_names
-        # for system_name in system_names:
-        #     start_snapshot = maintainability_portfolio_data.start_snapshot(system_name)
-        #     end_snapshot = maintainability_portfolio_data.end_snapshot(system_name)
-
-        #     print(f"{system_name} start: {start_snapshot}")
-        #     print(f"{system_name} end: {end_snapshot}")
-        #     break
-        # return parameter
-        # split_param = parameter['parameter'].split(":")
-        # pos_left = split_param[1]
-        # pos_top = split_param[2]
-        # pos_width = split_param[3]
-        pos_left = 0.56
-        pos_top = 3.5
-        pos_width = 9.57
-        pos_height = None
-        # if len(split_param) == 5:
-        #     pos_height = split_param[4]
-        portfolio = MaintainabilityPortfolioTreemapPlaceholder.create_portfolio()
-        treemap = MaintainabilityPortfolioTreemapPlaceholder.create_portfolio_treemap()
-
-        values = [None] * len(treemap['tracking'])
-        color_mapping = {}
-        for i, t in enumerate(treemap['tracking']):
-            cur_parent = treemap['parents'][i]
+    
+    def create_treemap(shape, portfolio, treemap_data, color_mapping):
+        pos_left = shape.left.inches
+        pos_top = shape.top.inches
+        pos_width = shape.width.inches
+        pos_height = shape.height.inches
+        
+        values = [None] * len(treemap_data['tracking'])
+        for i, t in enumerate(treemap_data['tracking']):
+            cur_parent = treemap_data['parents'][i]
             if cur_parent == "":
                 values[i] = 0
                 if t not in color_mapping.keys():
-                    color_mapping[t] = MaintainabilityPortfolioTreemapPlaceholder.SIG_BLUE
+                    color_mapping[t] = _AbstractTreemapPlaceholder.SIG_BLUE
                 continue
             values[i] = portfolio[t]['end_date_data']['volumeInPersonMonths']
-            color_mapping[t] = MaintainabilityPortfolioTreemapPlaceholder.translate_star_rating_to_color(portfolio[t]['end_date_data']['maintainability'])
-
-        fig = px.treemap(names=treemap['names'], parents=treemap['parents'], values=values, color=treemap['tracking'], color_discrete_map=color_mapping)
+        
+        fig = px.treemap(names=treemap_data['names'], parents=treemap_data['parents'], values=values, color=treemap_data['tracking'], color_discrete_map=color_mapping)
         fig.update_traces(root_color='rgba(250, 250, 250, 1)')
         fig.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+        
+        img_bytes = fig.to_image(format="jpg", width=pos_width*2*96,
+                                 height=pos_height*2*96)
 
-        img_bytes = fig.to_image(format="jpg", width=MaintainabilityPortfolioTreemapPlaceholder.TREEMAP_WIDTH_PIXELS,
-                                 height=MaintainabilityPortfolioTreemapPlaceholder.TREEMAP_HEIGHT_PIXELS)
+        return {
+            'pos_left' : Inches(pos_left),
+            'pos_top' : Inches(pos_top),
+            'pos_width' : Inches(pos_width),
+            'pos_height' : Inches(pos_height),
+            'img' : img_bytes
+        }
 
-        print(f"{pos_left} {pos_top} {pos_width} {pos_height}")
-        parameter['slide'].shapes.add_picture(io.BytesIO(img_bytes),
-            left=Inches(pos_left), top=Inches(pos_top),
-            width=Inches(pos_width))
-        # return {
-        #     'pos_left' : pos_left,
-        #     'pos_top' : pos_top,
-        #     'pos_width' : pos_width,
-        #     'pos_height' : pos_height,
-        #     'img' : img_bytes
-        # }
+
+class MaintainabilityPortfolioTreemapPlaceholder(_AbstractPortfolioPlaceholder):
+    key = "INTERVAL_PORTFOLIO_MAINTAINABILITY"
+
+    @classmethod
+    def value(cls, parameter=None):
+        shape = parameter['shape']
+        slide = parameter['slide']
+
+        portfolio = _AbstractPortfolioPlaceholder.create_portfolio()
+        treemap = _AbstractPortfolioPlaceholder.create_blank_portfolio_treemap()
+
+        color_mapping = {}
+        for t in portfolio.keys():
+            color_mapping[t] = _AbstractTreemapPlaceholder.translate_star_rating_to_color(portfolio[t]['end_date_data']['maintainability'])
+
+        data = _AbstractPortfolioPlaceholder.create_treemap(shape, portfolio, treemap, color_mapping)
+
+        slide.shapes.add_picture(io.BytesIO(data['img']),
+            left=data['pos_left'], top=data['pos_top'],
+            width=data['pos_width'], height=data['pos_height'])
+        
+        el = shape.element
+        el.getparent().remove(el)
+        return 0
+    
+class TestCodePortfolioTreemapPlaceholder(_AbstractPortfolioPlaceholder):
+    key = "INTERVAL_PORTFOLIO_TEST_CODE"
+
+    @classmethod
+    def value(cls, parameter=None):
+        shape = parameter['shape']
+        slide = parameter['slide']
+
+        portfolio = _AbstractPortfolioPlaceholder.create_portfolio()
+        treemap = _AbstractPortfolioPlaceholder.create_blank_portfolio_treemap()
+
+        color_mapping = {}
+        for t in portfolio.keys():
+            color_mapping[t] = _AbstractTreemapPlaceholder.translate_test_code_ratio_to_color(portfolio[t]['end_date_data']['testCodeRatio'])
+
+        data = _AbstractPortfolioPlaceholder.create_treemap(shape, portfolio, treemap, color_mapping)
+
+        slide.shapes.add_picture(io.BytesIO(data['img']),
+            left=data['pos_left'], top=data['pos_top'],
+            width=data['pos_width'], height=data['pos_height'])
+        
+        el = shape.element
+        el.getparent().remove(el)
         return 0
