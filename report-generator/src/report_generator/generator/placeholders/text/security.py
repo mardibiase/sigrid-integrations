cedacb655a47016a528c4368a7db07d5162d4f1e
maximjanssen <55410274+maximjanssen@users.noreply.github.com>
2026-03-20 09:58:11 +0100

    Add description

--- report-generator/src/report_generator/generator/placeholders/text/security.py
+++ report-generator/src/report_generator/generator/placeholders/text/security.py
@@ -33,6 +33,7 @@ def security_cvss_high():
 
 @text_placeholder()
 def security_acute_findings():
+    """The number of open acute security findings, (Critical and High severity findings)."""
     acute_fingings_count = security_data.count_findings('CRITICAL') + security_data.count_findings('HIGH')
     return f"{acute_fingings_count} Acute severity findings"
 
